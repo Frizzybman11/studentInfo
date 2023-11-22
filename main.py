87daa7674658b878e7534205223bd27ebe2d14f9
Frizzybman11 <Frizzybman11@aol.com>
2023-11-22 11:34:30 -0500

    Add ability to edit student entry
    Editing student entry will prompt for new information to be entered, then run update query

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def startMessage():
     print("2. Add/Edit a course entry")
     print("3. Add/Edit a score entry")
     user_input = input()
-    return user_input
+    menuSelect(user_input)
+    return
     
 def menuSelect(user_input):
     if user_input == "1":
@@ -16,11 +17,21 @@ def menuSelect(user_input):
         print("1. Add a new student")
         print("2. Edit a current student")
         print("3. View all student entries")
+        print("4. Return to main menu")
         user_input = input()
         if user_input == "1":
             addStudent()
         elif user_input == "2":
             editStudent()
+        elif user_input == "3":
+            viewStudents()
+            print("Press enter to return")
+            input()
+            user_input = "1"
+            menuSelect(user_input)
+        elif user_input == "4":
+            user_input = startMessage()
+            return
     elif user_input == "2":
         print("\u0332".join("Courses"))
         print("Please select a command:")
@@ -102,14 +113,146 @@ def addStudent():
             print("Sex must be male or female.")
     values = ", ".join(f"'{value}'" for value in newEntry)
     executeQuery(connection, create_student+"("+values+")")
-    print("Student entry added!")
-            
-    
-    
-    
+    print("Student entry added! Add another student? (y/n)")
+    user_input = input()
+    user_input = user_input.lower()
+    if user_input == "y" or user_input == "yes":
+        addStudent()
+        return
+    else:
+        user_input = "1"
+        menuSelect(user_input)
+               
     
 def editStudent():
-    print("WIP")
+    condition = True
+    print("Please enter a student ID to edit entry. For a list of students, type 'view'")
+    while(condition):
+        user_input = input()
+        if user_input.isalpha():
+            user_input = user_input.lower()
+            if user_input == "view" or user_input == "v":
+                viewStudents()
+                print("Please enter a student ID to edit entry.")
+                user_input = input()
+                if user_input.isnumeric():
+                    queryStudent(user_input)
+                    condition = False
+                else:
+                    print("Please enter a student ID number.")
+            else:
+                print("Please enter a student ID to edit entry. For a list of students, type 'view'")
+        elif user_input.isnumeric():
+            queryStudent(user_input)
+            condition = False  
+        else:
+            print("Please enter a student ID to edit entry. For a list of students, type 'view'")
+                                       
+def queryStudent(user_input):
+    first_name = ""
+    last_name = ""
+    grade = ""
+    sex = ""
+    query_list = []
+    student_entry = []
+    update_student = ""
+    condition = True
+    if user_input.isnumeric():
+        select_student = "SELECT * FROM students WHERE id = " + user_input
+        student = executeReadQuery(connection, select_student)
+        for entry in student:
+            student_entry = entry
+        print("First Name: " + student_entry[1])
+        print("Last Name: " + student_entry[2])
+        print("Grade: " + str(student_entry[3]))
+        print("Sex: " + student_entry[4])
+        print("For each field, enter new data. Leave blank to keep the same")
+        print("First Name: ")
+        while(condition):
+            user_input = input()
+            if not user_input.isalpha():
+                print("Name must contain alphabetic characters only.")
+            elif user_input:
+                first_name = user_input
+                condition = False
+            else:
+                first_name = student_entry[1]
+                condition = False
+        print("Last Name: ")
+        condition = True
+        while(condition):
+            user_input = input()
+            if not user_input.isalpha():
+                print("Name must contain alphabetic characters only.")
+            elif user_input:
+                last_name = user_input
+                condition = False
+            else:
+                last_name = student_entry[2]
+                condition = False
+        print("Grade: ")
+        condition = True
+        while(condition):
+            user_input = input()
+            if user_input.isnumeric():
+                user_input = int(user_input)
+                if not user_input >= 1 and not user_input <= 6:
+                    print("Grade must be between 1st and 6th.")
+                elif user_input:
+                    grade = user_input
+                    condition = False
+                else:
+                    grade = student_entry[3]
+                    condition = False
+            else: 
+                print("Grade must be numerical.")
+        print("Sex: ")
+        condition = True
+        while(condition):
+            user_input = input()
+            if user_input.isalpha():
+                user_input = user_input.lower()
+                if user_input == "m" or user_input == "male":
+                    sex = "M"
+                    condition = False
+                elif user_input == "f" or user_input == "female":
+                    sex = "F"
+                    condition = False
+                elif user_input:
+                    print("Sex must be male or female.")
+                else:
+                    sex = student_entry[4]
+                    condition = False     
+            else:
+                print("Sex must be male or female.")
+        update_student = """
+        UPDATE
+            students
+        SET
+            fname = """ + '"' + first_name + '"' """,
+            lname = """ + '"' + last_name + '"' """,
+            grade = """ + '"' + str(grade) + '"' """,
+            sex = """ + '"' + sex + '"' """
+        WHERE
+            id = """ + str(student_entry[0]) + """
+        """
+        try:
+            executeQuery(connection, update_student)
+            print("Entry updated successfully!")
+            print("Press enter to return")
+            input()
+            user_input = "1"
+            menuSelect(user_input)
+        except Error as e:
+            print(f"The error '{e}' occurred.")
+                   
+    
+def viewStudents():
+    select_students = "SELECT * from students"
+    students = executeReadQuery(connection, select_students)
+    
+    for student in students:
+        print(student)
     
 def addCourse():
     print("WIP")
@@ -138,6 +281,16 @@ def executeQuery(connection, query):
         connection.commit()
     except Error as e:
         print(f"The error '{e}' occurred.")
+        
+def executeReadQuery(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f"The error '{e}' occurred")
 
 connection = createConnection("./student_info.sqlite")
 create_students_table = """
@@ -191,5 +344,4 @@ executeQuery(connection, create_students_table)
 executeQuery(connection, create_courses_table)
 executeQuery(connection, create_scores_table)
 
-user_input = startMessage()
-user_input = menuSelect(user_input)
+startMessage()
